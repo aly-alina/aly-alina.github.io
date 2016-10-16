26510bcde46f626f5a30bf15b0b418ef62fe1b4d
aly-alina <alina.aleshkina.01@gmail.com>
2016-10-16 15:13:18 +0800

    Bug fix

--- js/game.js
+++ js/game.js
@@ -7,7 +7,7 @@ var canvas = document.getElementById("gameCanvas");
 var canvasCtx = canvas.getContext("2d");
 
 var ball = {
-    radius: 17,
+    radius: 12,
     x: canvas.width / 2,
     y: 0,
     xSpeed: 2,
@@ -19,10 +19,11 @@ ball.y = canvas.height - ball.radius * 3;
 var paddle = {
     width: 90,
     height: 13,
-    x: canvas.width / 2,
+    x: 0,
     color: "#558651",
     speed: 5
 }
+paddle.x = canvas.width / 2 - paddle.width / 2;
 
 var keys = {
     rightPressed: false,
@@ -30,38 +31,34 @@ var keys = {
 }
 
 var keyDownHandler = function(e) {
-    if (e.keyCode == 39) {
+    if (e.keyCode == 39)
         keys.rightPressed = true;
-        console.log('hit');
-    }
     else if (e.keyCode == 37)
         keys.leftPressed = true;
 }
 
 var keyUpHandler = function(e) {
-    if (e.keyCode == 39) {
+    if (e.keyCode == 39)
         keys.rightPressed = false;
-        console.log('up');
-    }
     else if (e.keyCode == 37)
         keys.leftPressed = false;
 }
 
 var draw = function() {
     canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
     drawPaddle();
+    drawBall();
 }
 
 var drawPaddle = function() {
     canvasCtx.beginPath();
-    canvasCtx.rect(paddle.x - paddle.width / 2, canvas.height - paddle.height, paddle.width, paddle.height);
+    canvasCtx.rect(paddle.x, canvas.height - paddle.height, paddle.width, paddle.height);
     canvasCtx.fillStyle = paddle.color;
     canvasCtx.fill();
     canvasCtx.closePath;
-    if (keys.rightPressed && paddle.x < canvas.width - paddle.width/2)
+    if (keys.rightPressed && paddle.x + paddle.width < canvas.width)
         paddle.x += paddle.speed;
-    if (keys.leftPressed && paddle.x > paddle.width/2)
+    if (keys.leftPressed && paddle.x > 0)
         paddle.x -= paddle.speed;
 }
 
@@ -72,16 +69,20 @@ var drawBall = function() {
     canvasCtx.fill();
     canvasCtx.closePath();
     
-    if (ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0)
+    if (ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0) {
         ball.xSpeed = -ball.xSpeed;
-    if (ball.y - ball.radius < 0)
+    }   
+    if (ball.y - ball.radius < 0) {
         ball.ySpeed = -ball.ySpeed;
-    else if (ball.y + ball.radius > canvas.height - paddle.height) {
-        if (ball.x > (paddle.x - paddle.width/2) && ball.x < (paddle.x + paddle.width/2))
+    } else if (ball.y + ball.ySpeed >= canvas.height - ball.radius - paddle.height) {
+        if (ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
             ball.ySpeed = -ball.ySpeed;
+        }
         else {
+            console.log(ball.x);
+            console.log(paddle.x);
             alert("Game ooooover");
-            document.location.reload();
+            document.location.reload();  
         }
     }
     
